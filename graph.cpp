b619946362b447867ec518ef0fd50eb168e47d3e
Ilya <mokill44@gmail.com>
2022-05-18 09:29:05 +0300

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <queue>
 
 using namespace std;
@@ -15,6 +16,45 @@ struct Graph {
 	vector<vector<Edge>> edges; // Внешний вектор отвечает за вершины
 	// vector<Edge> хранит набор ребер исходящих из вершины с номером i
 };
+
+int min_white_v(const vector<int> &D, const vector<int> &C){
+	int min = 1e9;
+	int min_index = -1;
+	for (int i =0; i < D.size(); i++){
+		if (D[i]<min  && C[i]==0){
+			min = D[i];
+			min_index = i;
+		}
+	}
+	return min_index;
+}
+
+void Dijkstra(const Graph & G,  vector<int> &C, 
+			vector<int> &P, vector<int> &D, int start )
+{
+	for (int i =0; i<G.v_count; i++) {
+		C[i]=0;
+		P[i] = -2;
+		D[i] = 1e9;
+	}
+	P[start] = -1;
+	D[start] = 0;
+	int cur ;
+	for (int i = 0; i < G.v_count; i++){
+		cur = min_white_v(D,C);
+		if (cur == -1) break;
+		for (int j =0; j<G.edges[cur].size(); j++){
+			int to = G.edges[cur][j].to;
+			if (C[to] == 0){
+				if (D[to]> D[cur]+ G.edges[cur][j].weight ){
+					D[to] = D[cur]+ G.edges[cur][j].weight;
+					P[to] = cur;
+				}
+			}
+		}
+		C[cur] = 1;
+	}
+}
 
 void BFS (const Graph & G, vector<int> &C, 
 			vector<int> &P, vector<int> &D, int start)
@@ -76,4 +116,47 @@ void readMatrix( Graph & g);
 void fillMatrix( Graph & g);
 void fillEdges( Graph & g);
 
-int main() {}
+int main() {
+	int N,x;
+	Edge E;
+	Edge cur ;
+	N = 6;
+	vector<int> C (N,0), D(N, -1), P (N,-2);
+	Graph G;
+	G.v_count = N;
+	G.edges.resize(N); 
+	E.from = 0;
+	E.to= 2;
+	E.weight = 1;
+	G.edges[0].push_back(E);
+	E.from = 0;
+	E.to= 1;
+	E.weight = 1;
+	G.edges[0].push_back(E);
+	E.from = 0;
+	E.to= 4;
+	E.weight = 3;
+	G.edges[0].push_back(E);
+	E.from = 1;
+	E.to= 4;
+	E.weight = 1;
+	G.edges[1].push_back(E);
+	E.from = 1;
+	E.to= 2;
+	E.weight = 3;
+	G.edges[1].push_back(E);
+	Dijkstra(G,C,P,D,0);
+	for(auto x : D) {cout<<x<<endl;}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
